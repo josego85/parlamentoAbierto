4945ae19c2ac94a55bcde5b75c5245efb7a70a26
proyectosbeta <bauerpy@gmail.com>
2014-09-20 10:09:46 -0400

    bla bla

--- public/parseador/prueba.php
+++ public/parseador/prueba.php
@@ -6,10 +6,10 @@ require "php_ft_lib.php";
 require "constantes.php";
 
 
-$tableid = '';
-$username = "";
-$password = "";
-$key = "";
+$tableid = '1NI2gWkeW9ZyZiQ2TmenBZr23vcUMlZFzN9KO5Zgf';
+$username = "bauerpy@gmail.com";
+$password = "chmod 777 www";
+$key = "AIzaSyDXCk5wXDCvckFWZb2PLAT6PwxFkoUcmVM";
 
 
 $token = GoogleClientLogin($username, $password, "fusiontables"); 
@@ -18,10 +18,10 @@ $token = GoogleClientLogin($username, $password, "fusiontables");
 $ft = new FusionTable($token, $key); 
 
 //ejemplo insert
-$result = $ft->query("INSERT INTO $tableid (Text) VALUES ('kat')"); 
-
+$result = $ft->query("INSERT INTO $tableid (asuntoId) VALUES (2)"); 
+print_r($result);
 
 //ejemplo select
-$output = $ft->query("SELECT * FROM $tableid"); 
-
+$output = $ft->query("SELECT * FROM $tableid");
+print_r($output);
 
