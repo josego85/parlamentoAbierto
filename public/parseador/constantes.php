654f7b18abe53b5df4ce69548d4abeb3951ff8fe
proyectosbeta <josego85@gmail.com>
2014-09-20 14:48:25 -0500

    se borraron constantes que ya no se usan mas.

--- public/parseador/constantes.php
+++ public/parseador/constantes.php
@@ -8,7 +8,3 @@
 	define("NOMBRE_TABLA_BLOQUE_DIPUTADOS", "1MP1Jt-K05GNNizRGo1NIXeKLFA8FoUn3faAP87XJ");
 	define("NOMBRE_TABLA_ASUNTOS_DIPUTADOS", "1p1tUu1rYARXwjETyryDho2auAat0fwX9j2X4tkQp");
 	define("NOMBRE_TABLA_VOTACIONES_DIPUTADOS", "1NI2gWkeW9ZyZiQ2TmenBZr23vcUMlZFzN9KO5Zgf");
-	
-	// Cuenta de Google
-	define("USERNAME_GOOGLE", "parlamentoabiertopy@gmail.com");
-	define("PASSWORD_USERNAME_GOOGLE", "@,6//5;bST");
