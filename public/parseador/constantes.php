979f07507185f80fabae799af3c81a23ce5b93c3
josego85@gmail.com <josego85@gmail.com>
2015-01-17 21:07:35 -0300

    Se agrego una constante que se habia perdido

--- public/parseador/constantes.php
+++ public/parseador/constantes.php
@@ -2,13 +2,16 @@
     // Constantes
     // API Key de Google Table Fusion.
     //define("API_KEY_GOOGLE_TABLE_FUSION", "AIzaSyAOyW19eQZ2aJ7DSNHFzLfuuZW33zZUg2g");
-	
+
     // Nombre de tablas de Google Table Fusion.
     define("NOMBRE_TABLA_DIPUTADOS", "diputados");
     define("NOMBRE_TABLA_BLOQUE_DIPUTADOS", "bloques_diputados");
     define("NOMBRE_TABLA_ASUNTOS_DIPUTADOS", "asuntos_diputados");
     define("NOMBRE_TABLA_VOTACIONES_DIPUTADOS", "votaciones_diputados");
-    
+
     // Base de datos.
     define("BD", "votacionespa");
     define("HOST", "localhost");
+
+    // Path de los archivos que se generan.
+    define("PATH_ARCHIVOS_GENERADOS", "/www/votaciones/parlamentoAbierto/public/archivosGenerados/");
