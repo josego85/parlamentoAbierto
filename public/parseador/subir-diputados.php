7a374ccbaa6b39dce6cb24d7d24195cfddb82fd9
josego85@gmail.com <josego85@gmail.com>
2015-01-18 00:45:05 -0300

    correcion de un bug

--- public/parseador/subir-diputados.php
+++ public/parseador/subir-diputados.php
@@ -1,43 +1,42 @@
 <?php
-ini_set('display_errors', 1); // para ke te haga display de todos los errores
-require_once  'constantes.php';
-if(!session_id()){
-    session_start();
-}
-require 'conexion.php';
-if(!$_SESSION['logged']){
-    header('Location: '.'salir.php');
-    exit;
-}
-echo "
-<a href='salir.php'>Salir</a>
-<center>
-    <br>
-    <br>
-    <h1>Subir votos diputados</h1>
+    require_once  'constantes.php';
+    require 'conexion.php';
+    if(!session_id()){
+        session_start();
+    }
+    if(!$_SESSION['logged']){
+        header('Location: '.'salir.php');
+        exit;
+    }
+    echo "
+        <a href='salir.php'>Salir</a>
+        <center>
+            <br>
+            <br>
+            <h1>Subir votos diputados</h1>
 	<form action='parseador.php' method='post' enctype='multipart/form-data'>
-	<input type='file' name='votacion' />
-	Resultado:
-	<select name='resultado'>
-	    <option value='AFIRMATIVO'>AFIRMATIVO</option>
-	    <option value='NEGATIVO'>NEGATIVO</option>
-	    <option value='ANULADA'>ANULADA</option>
-	</select>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Presidente:
-	<select name='presidente'>";
-            $result = mysql_query("SELECT * FROM diputados order by nombre");
-            $data = array();
-            echo "<option value='' selected='selected'></option>";
-            while ($fila = mysql_fetch_assoc($result)) {
-                echo "<option value='".$fila['nombre']."']'>".$fila['nombre']."</option>";
-            }
-                echo "
-	</select>
-    	<select name='votopresidente'>
-	    <option value='0' >AFIRMATIVO</option>
-	    <option value='1'>NEGATIVO</option>
-	    <option value='' selected='selected'>No vot&oacute;</option>
-	</select>
-	<input type='submit' value='Subir' onclick='this.value='Enviando...'; this.disabled='disabled'; this.form.submit();'/>
+	    <input type='file' name='votacion' />
+	    Resultado:
+	    <select name='resultado'>
+	        <option value='AFIRMATIVO'>AFIRMATIVO</option>
+	        <option value='NEGATIVO'>NEGATIVO</option>
+	        <option value='ANULADA'>ANULADA</option>
+	    </select>
+	    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Presidente:
+	    <select name='presidente'>";
+                $result = mysql_query("SELECT * FROM diputados order by nombre");
+                $data = array();
+                 echo "<option value='' selected='selected'></option>";
+                 while ($fila = mysql_fetch_assoc($result)) {
+                    echo "<option value='".$fila['nombre']."''>".$fila['nombre']."</option>";
+                 }
+                 echo "
+	     </select>
+    	         <select name='votopresidente'>
+	         <option value='0' >AFIRMATIVO</option>
+	         <option value='1'>NEGATIVO</option>
+	         <option value='' selected='selected'>No vot&oacute;</option>
+	    </select>
+	    <input type='submit' value='Subir' onclick='this.value='Enviando...'; this.disabled='disabled'; this.form.submit();'/>
 	</form>
-</center>";
+        </center>";
